38ad71549f68736a37bbec0f2fea2a6b9087c0a5
Yerassyl Saiman <y_saiman@kbtu.kz>
2024-06-22 01:22:51 +0500

    Initial Commit

---
+++ quanta-ios-task/quanta_ios_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  quanta_ios_taskApp.swift
+//  quanta-ios-task
+//
+//  Created by Yerasyl on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct quanta_ios_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
